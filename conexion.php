a81dd40c926e5d34642c2b1757ddf5020d8c8cf7
AnnieCarrillo <annyevans135n8@gmail.com>
2023-05-24 08:11:23 -0600

    Updated

---
+++ conexion.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$nombre_usuario = $_POST['nombre_usuario'];
+$contraseña = $_POST['contraseña'];
+
+
+    $servidor='localhost';
+    $usuario='annie';
+    $clave='root';
+    $BD='id20739455_progweb2';
+
+    $conection=mysqli_connect($servidor, $usuario, $clave, $BD);
+
+    $consulta ="SELECT * FROM alumnos WHERE nombre_usuario= '$nombre_usuario' and contraseña='$contraseña'";
+    $resultado = mysqli_query($conection, $consulta);
+    
+    $filas = mysqli_num_rows($resultado);
+    
+    if ($filas > 0) {
+        $fila = mysqli_fetch_assoc($resultado);
+        $no_cuenta = $fila['no_cuenta'];
+
+        $_SESSION['no_cuenta'] = $no_cuenta;
+    
+        header("location: template.php");
+        exit();
+    } else {
+        echo "Error en la autentificación";
+    }
+     
+    mysqli_free_result($resultado);
+    mysqli_close($conection);
+
+    
+}
+
+?>
