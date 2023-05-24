f4d8fe42466d04e09d12bf2a0f7f93ef272db67d
AnnieCarrillo <annyevans135n8@gmail.com>
2023-05-24 08:52:03 -0600

    Updated

---
+++ eliminar.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $no_cuenta = $_POST['no_cuenta'];
+    $nuevo_telefono = $_POST['nuevo_telefono'];
+    $nuevo_correo = $_POST['nuevo_correo'];
+
+    $servidor = 'localhost';
+    $usuario = 'annie';
+    $clave = 'root';
+    $BD = 'id20739455_progweb2';
+
+    $connection = mysqli_connect($servidor, $usuario, $clave, $BD);
+
+    if (!empty($nuevo_telefono) && !empty($nuevo_correo)) {
+        $consulta = "UPDATE alumnos SET telefono = '$nuevo_telefono', correo = '$nuevo_correo' WHERE no_cuenta = '$no_cuenta'";
+    } elseif (!empty($nuevo_telefono)) {
+        $consulta = "UPDATE alumnos SET telefono = '$nuevo_telefono' WHERE no_cuenta = '$no_cuenta'";
+    } elseif (!empty($nuevo_correo)) {
+        $consulta = "UPDATE alumnos SET correo = '$nuevo_correo' WHERE no_cuenta = '$no_cuenta'";
+    } else {
+        echo "No se especificaron campos para modificar.";
+        exit;
+    }
+
+    $resultado = mysqli_query($connection, $consulta);
+
+    if ($resultado) {
+        echo "Registro modificado correctamente. <br>";
+        echo '<a href="template.php">Volver a la página</a>';
+    } else {
+        echo "Error al modificar el registro.";
+    }
+
+    mysqli_close($connection);
+}
+?>
---
+++ modificar.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $no_cuenta = $_POST['no_cuenta'];
+
+    $servidor = 'localhost';
+    $usuario = 'annie';
+    $clave = 'root';
+    $BD = 'id20739455_progweb2';
+
+    $connection = mysqli_connect($servidor, $usuario, $clave, $BD);
+
+    $consulta = "DELETE FROM alumnos WHERE no_cuenta = '$no_cuenta'";
+    $resultado = mysqli_query($connection, $consulta);
+
+    if ($resultado) {
+        echo "Registro eliminado correctamente.";
+        echo <a href="template.php">Volver a la página</a>;
+    } else {
+        echo "Error al eliminar el registro.";
+    }
+
+    mysqli_close($connection);
+}
+?>
